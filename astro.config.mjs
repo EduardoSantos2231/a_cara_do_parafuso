4f9697d54e834656e9f2a743dc5dfd1afb1afebe
Eduardo Nascimento <eduardonascimento2231@gmail.com>
2026-09-12 21:07:17 -0300

    Configure site and GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eduardosantos2231.github.io',
+  base: '/a_cara_do_parafuso',
+});
